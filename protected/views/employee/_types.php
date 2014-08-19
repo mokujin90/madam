9d622c1e83e8980052f2977614058884018326bf
multeg <multik@nxt.ru>
2014-08-19 20:08:13 +0400

    Issue: all answer for one user

--- protected/views/employee/_types.php
+++ protected/views/employee/_types.php
@@ -1,36 +1,33 @@
 <div class="col-xs-12 col-sm-8">
-    <div class="hidden form-group">
+    <div class="form-group">
         <label class="col-xs-4 control-label">Ответы, одобрены для графика</label>
         <div class="col-xs-8">
             <div class="radio">
-                <label>
-                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
-                    Все ответы
-                </label>
+                <?php echo CHtml::radioButton('User[all_answers]',$model->all_answers==1?true:false,array('id'=>'option_all_answer','value'=>1,'class'=>'user-type-answer'))?>
+                <?php echo CHtml::label(Yii::t('main','Все ответы'),'option_all_answer');?>
             </div>
             <div class="radio">
-                <label>
-                    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-                    Определенные ответы
-                </label>
+                <?php echo CHtml::radioButton('User[all_answers]',$model->all_answers!=1?true:false,array('id'=>'optionsRadios2','value'=>0,'class'=>'user-type-answer'))?>
+                <?php echo CHtml::label(Yii::t('main','Определенные ответы'),'optionsRadios2');?>
             </div>
         </div>
     </div>
+    <div id="user-answer" style="<?if($model->all_answers==1):?>display: none;<?endif?>">
     <?foreach($question as $item):?>
-    <div class="form-group">
-        <label class="col-xs-4 control-label"><?=$item->text?></label>
-        <div class="col-xs-8">
-            <?foreach($item['answers'] as $answer):?>
-            <div class="checkbox">
-                <label>
-                    <?=CHtml::checkBox('question['.$answer->id.'][]',isset($user2answer[$answer->id])?true:false)?><?=$answer->text?>
-                </label>
+        <div class="form-group">
+            <label class="col-xs-4 control-label"><?=$item->text?></label>
+            <div class="col-xs-8">
+                <?foreach($item['answers'] as $answer):?>
+                <div class="checkbox">
+                    <label>
+                        <?=CHtml::checkBox('question['.$answer->id.'][]',isset($user2answer[$answer->id])?true:false)?><?=$answer->text?>
+                    </label>
+                </div>
+                <?endforeach?>
             </div>
-            <?endforeach?>
         </div>
+        <?endforeach;?>
     </div>
-    <?endforeach;?>
-
 </div>
 <div class="col-xs-12 col-sm-4">
 
