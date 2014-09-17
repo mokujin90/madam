9785a66d4dfc5b85a146808eae7cb1e35f86cebb
mokujin <mokujin@inbox.ru>
2014-09-18 00:18:49 +0400

    fix'

--- protected/commands/ReminderCommand.php
+++ protected/commands/ReminderCommand.php
@@ -9,16 +9,16 @@ class ReminderCommand extends CConsoleCommand
         $criteria->addCondition('alarm_time > 1');
         $model = Request::model()->findAll($criteria);
         $dateStart = clone $dateNow;
-        $dateStart->modify('-50000 minutes');
+        $dateStart->modify('-5 minutes');
         $dateEnd = clone $dateNow;
-        $dateEnd->modify('+50000 minutes');
+        $dateEnd->modify('+5 minutes');
         foreach ($model as $item) {
             $dateVal = new DateTime($item->start_time);
             $dateVal->modify("- {$item->alarm_time} hours");
             if ($dateStart < $dateVal && $dateVal < $dateEnd) {
                 $license = Company2License::getCurrentLicense($item->user->company_id);
                 if ($license->license->email_reminder) {
-                    Help::sendMail($item->getEmailField(), 'Напоминание о termin', 'reminder', $item);
+                    Help::sendMail($item->getEmailField(), 'Напоминание о termin', 'reminder', $item, $this);
                     $item->alarm_time = -1;
                     $item->save(false);
                     echo 'success';
